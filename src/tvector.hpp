7c0c9279027daf7521f6e26f54f0821631c726f8
Samuel Olivier <smsolivier@gmail.com>
2024-01-19 16:42:32 -0700

    added const version of mdspan views for transport vector

--- src/tvector.hpp
+++ src/tvector.hpp
@@ -16,10 +16,12 @@ using TransportVectorLayout = Kokkos::layout_right;
 // index into transport vector with (energy, angle, space) 
 using TransportVectorExtents = Kokkos::dextents<std::size_t,3>; 
 using TransportVectorView = Kokkos::mdspan<double,TransportVectorExtents,TransportVectorLayout>; 
+using ConstTransportVectorView = Kokkos::mdspan<const double,TransportVectorExtents,TransportVectorLayout>; 
 
 // index into moment vector with (moment number, energy, space)
 using MomentVectorExtents = Kokkos::dextents<std::size_t,3>;
 using MomentVectorView = Kokkos::mdspan<double,MomentVectorExtents,Kokkos::layout_right>;  
+using ConstMomentVectorView = Kokkos::mdspan<const double,MomentVectorExtents,Kokkos::layout_right>;
 
 template<typename T, std::size_t... Extents>
 T TotalExtent(const Kokkos::extents<T,Extents...> &ext) {
