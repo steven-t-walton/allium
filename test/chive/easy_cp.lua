2e9aea133f51e15641f76e4de800a9102fd81858
Samuel Olivier <smsolivier@gmail.com>
2024-03-06 13:09:06 -0700

    add GCI tests for crooked pipe

---
+++ test/chive/easy_cp.lua
@@ -0,0 +1,87 @@
+sig_max = 2
+sig_min = 0.2
+pipe = {
+	total = sig_min, 
+	scattering = sig_min - 1e-3, 
+	source = 1e-7
+}
+
+wall = {
+	total = sig_max, 
+	scattering = sig_max - 1e-3, 
+	source = 1e-7
+}
+
+materials = {pipe = pipe, wall = wall}
+
+function material_map(x,y,z) 
+	r = "wall"
+	if (y<0.5) then 
+		r = "pipe"
+	end
+	if (x > 2.5 and x < 4.5 and y<1.5) then 
+		r = "pipe"
+	end
+	if (x > 3 and x < 4 and y < 1) then
+		r = "wall"
+	end 
+	return r 
+end 
+
+boundary_conditions = {
+	pipe = {
+		type = "inflow", 
+		value = 1.0/2/math.pi
+	},
+	wall = {
+		type = "vacuum", 
+	}, 
+	bottom = {
+		type = "reflective"
+	}
+}
+
+function boundary_map(x,y,z)
+	r = "wall" 
+	if (x < 1e-10 and y < 0.5) then 
+		r = "pipe"
+	end 
+	if (y<1e-10) then 
+		r = "bottom"
+	end
+	return r 
+end 
+
+mesh = {
+	num_elements = {14,4},
+	extents = {7,2},
+	sfc_ordering = false,
+	serial_refinements = 0,
+	parallel_refinements = 0,
+}
+
+cg_solver = {
+	type = "cg", 
+	abstol = 1e-7, 
+	max_iter = 100, 
+	iterative_mode = false,
+}
+
+driver = {
+	fe_order = 1, 
+	sn_order = 8,
+	basis_type = "lobatto",
+	solver = {
+		type = "gmres", 
+		abstol = 1e-5, 
+		max_iter = 300, 
+		kdim = 10, 
+		iterative_mode = false
+	},
+}
+
+output = {
+	tracer = {
+		{7,0}, 
+	}
+}
---
+++ test/chive/test_gci.py
@@ -0,0 +1,45 @@
+import math
+import subprocess 
+import yaml 
+import argparse 
+from collections import defaultdict 
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-e', '--exe', type=str, help='path to executable', required=True)
+parser.add_argument('-i', '--input', type=str, help='path to base input file', required=True)
+parser.add_argument('-l', '--lua', type=str, help='extra lua commands for test', required=True)
+parser.add_argument('-p', '--fe_order', type=int, help='finite element order', default=1)
+parser.add_argument('-r', '--base_ref', type=int, help='starting refinement level', default=0)
+parser.add_argument('-n', '--nprocs', type=int, help='number of processors to use', default=1)
+parser.add_argument('-o', '--expected_order', type=float, help='expected order of accuracy', required=True)
+args = parser.parse_args()
+
+d = []
+for r in range(3):
+	cmd = ['mpirun', '-n', str(args.nprocs), args.exe, '-i', args.input, 
+		'-l', f'\"driver.fe_order = {args.fe_order}; mesh.parallel_refinements = {r+args.base_ref}; {args.lua}\"']
+	print(*cmd)
+	res = subprocess.run(cmd, stdout=subprocess.PIPE, text=True)
+	print(res.stdout)
+	db = yaml.safe_load(res.stdout)
+	tracers = db['output']['tracer']
+	local = []
+	for tracer in tracers:
+		local.append(tracer['scalar flux'])
+	d.append(local)
+
+print(d) 
+at_least_one = False
+for i in range(len(d[0])):
+	f1 = d[0][i]
+	f2 = d[1][i] 
+	f3 = d[2][i]
+	try:
+		gci = math.log2( (f1 - f2)/(f2 - f3) )
+		print(f'{gci:.3f}')
+		assert(math.fabs(gci - args.expected_order) < 0.3)
+		at_least_one = True
+	except:
+		print('log failed')
+
+assert(at_least_one)
