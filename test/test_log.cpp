79d118432ed31293751353a40e734c330c03881d
Samuel Olivier <smsolivier@gmail.com>
2024-04-24 16:52:38 -0600

    added test for logger

---
+++ test/test_log.cpp
@@ -0,0 +1,49 @@
+#include "gtest/gtest.h"
+#include "mfem.hpp"
+#include "log.hpp"
+#include <map>
+#include <variant>
+
+TEST(EventLog, Synchonize) {
+	const auto np = mfem::Mpi::WorldSize(); 
+	const auto rank = mfem::Mpi::WorldRank(); 
+	EventLog["world size"] = 1; 
+	if (rank%2) EventLog["only on even"] = 1; 
+	EventLog.Synchronize(); 
+	if (mfem::Mpi::Root()) {
+		EXPECT_EQ(EventLog["world size"], np); 
+		EXPECT_EQ(EventLog["only on even"], np/2); 
+	}
+}
+
+template<typename T>
+class Dictionary 
+{
+private:
+	std::map<std::string,std::unique_ptr<Dictionary>> map;
+	T data; 
+public:
+	Dictionary &operator[](std::string str) {
+		auto it = map.find(str); 
+		Dictionary *dict_ptr;
+		if (it == map.end()) {
+			auto uptr = std::make_unique<Dictionary>(); 
+			dict_ptr = uptr.get(); 
+			map[str] = std::move(uptr); 
+		} else {
+			dict_ptr = it->second.get(); 
+		}
+		return *dict_ptr; 
+	}
+	T &operator*() { return data; }
+	T *operator->() { return &data; }
+	void operator=(T value) { data = value; }
+};
+
+TEST(IterLog, test) {
+	Dictionary<int> dct; 
+	dct["test"] = 5; 
+	std::cout << "value = " << *dct["test"] << std::endl; 
+	Dictionary<mfem::Array<int>> array; 
+	array->SetSize(5); 
+}
